dbfd1f5b59c444ca2831cc05cef9196dd6e91985
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-01-30 20:24:12 -0300

    criando metodo da TaskService parar criar task

--- src/main/java/com/cyberenjeru/todolist/service/TaskService.java
+++ src/main/java/com/cyberenjeru/todolist/service/TaskService.java
@@ -1,12 +1,19 @@
 package com.cyberenjeru.todolist.service;
 
+import com.cyberenjeru.todolist.model.Task;
 import com.cyberenjeru.todolist.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
 
+    @Autowired
     private TaskRepository taskRepository;
+
+    public Task createTask (Task task){
+        return taskRepository.save(task);
+    }
 }
