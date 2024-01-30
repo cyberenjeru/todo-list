653d8953331fae29ed62a52ac4eea05cef29a859
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-01-30 20:25:56 -0300

    chamando o TaskService no TaskController para separar em camadas

--- src/main/java/com/cyberenjeru/todolist/controller/TaskController.java
+++ src/main/java/com/cyberenjeru/todolist/controller/TaskController.java
@@ -1,4 +1,12 @@
 package com.cyberenjeru.todolist.controller;
 
+import com.cyberenjeru.todolist.service.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+
+    TaskService taskService;
 }
