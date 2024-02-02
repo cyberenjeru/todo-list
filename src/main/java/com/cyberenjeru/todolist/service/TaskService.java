eae002c489800ab66c18cb7231822ba3ce6e69d1
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-02-01 23:18:17 -0300

    implementando método de delete das tasks

--- src/main/java/com/cyberenjeru/todolist/service/TaskService.java
+++ src/main/java/com/cyberenjeru/todolist/service/TaskService.java
@@ -39,4 +39,12 @@ public class TaskService {
                     return ResponseEntity.ok().body(update);
                 }).orElse(ResponseEntity.notFound().build());
     }
+
+    public ResponseEntity<Object> deleteTaskById(Long id){
+        return  taskRepository.findById(id)
+                .map(taskToDelete ->{
+                    taskRepository.deleteById(id);
+                    return ResponseEntity.noContent().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
