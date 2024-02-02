7de4d096489c4c13919e58a81e570fe233e6a372
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-02-02 13:35:22 -0300

    refatorando taskcontroller

--- src/main/java/com/cyberenjeru/todolist/controller/TaskController.java
+++ src/main/java/com/cyberenjeru/todolist/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.cyberenjeru.todolist.controller;
 import com.cyberenjeru.todolist.model.Task;
 import com.cyberenjeru.todolist.service.TaskService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 @AllArgsConstructor
+@Slf4j
 public class TaskController {
 
 
@@ -21,30 +23,37 @@ public class TaskController {
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.CREATED)
     public Task createTask(@RequestBody Task task){
+
+        log.info("Criando tarefa [{}]", task);
         return taskService.createTask(task);
     }
 
     @GetMapping("/tasks")
     @ResponseStatus(HttpStatus.OK)
     public List<Task> getAllTasks(){
+
+        log.info("Listando todas as tarefas cadastradas");
         return taskService.listAllTasks();
     }
 
     @GetMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> getTaskById(@PathVariable(value = "id")Long id){
+        log.info("Buscando tarefa [{}]", id);
         return taskService.findTaskById(id);
     }
 
     @PutMapping("tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable(value = "id") Long id,@RequestBody Task task){
+        log.info("Atualidando tarefa [{}] com as informações [{}]", id, task);
         return taskService.updateTaskById(task,id);
     }
 
     @DeleteMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> deleteTaskById(@PathVariable(value = "id")Long id){
+        log.info("Deletando a tarefa [{}]", id);
         return taskService.deleteTaskById(id);
     }
 }
